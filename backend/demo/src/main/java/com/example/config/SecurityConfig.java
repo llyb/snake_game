6b317ff425354843647800f23807825258304afa
Ethreal <1615115712@qq.com>
2023-07-15 22:25:21 +0800

    测试mybatis-plus并且简单实现spring-security

---
+++ backend/demo/src/main/java/com/example/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() { // security在加密的时候肯定会调用该方法
+        return new BCryptPasswordEncoder(); // 使用该加密方法
+    }
+}
--- backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -10,4 +12,8 @@ class DemoApplicationTests {
     void contextLoads() {
     }
 
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("lyb"));
+    }
 }
